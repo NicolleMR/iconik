d662a93a068b2ca795cfcd5930e0a4cd8627c506
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-13 23:21:04 -0500

    main grid styles and fix tab sizes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,15 @@
-const App = () => {
-    return (
-      <div>
-        Iconik
-      </div>
-    );
-  }
+import Header from "../Header";
+import NavBar from "../NavBar/NavBar";
+import "./app.css";
+
+const App = () => (
+  <main className="main">
+    <NavBar/>
+    <Header/>
+    <div className="main__content">
+      content  
+    </div>
+  </main>
+);
   
 export default App;
