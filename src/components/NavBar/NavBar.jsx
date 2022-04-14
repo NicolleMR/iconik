42527679f3cb35dd34a886822903e73fad334280
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-13 23:15:38 -0500

    Create nav bar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import iconikLogo from "../../assets/images/iconik-logo.png";
+import "./nav-bar.css";
+
+const NavBar = () => (
+  <div className="nav-bar">
+    <img className="nav-bar__logo" src={iconikLogo} alt="Iconik"/>
+    <nav className="nav-bar__menu">
+      <a className="nav-bar__link is-active" href="#">Ingresos</a>
+      <a className="nav-bar__link" href="#">Egresos</a>
+      <a className="nav-bar__link" href="#">Trabajador/a</a>
+      <a className="nav-bar__link" href="#">Clientes</a>
+      <a className="nav-bar__link" href="#">Citas</a>
+      <a className="nav-bar__link" href="#">Productos</a>
+      <a className="nav-bar__link" href="#">Configuración</a>
+    </nav>
+  </div>
+);
+
+export default NavBar;
