938858d502dd8ed2c413fdf9217fce3b74f2f4c1
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-13 22:50:48 -0500

    Create header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css";
+
+const Header = () => (
+  <header className="header">
+    <div className="header__user">Nicolle Montilla R.</div>
+    <button className="header__button">Salir</button>
+  </header>
+);
+
+export default Header;
