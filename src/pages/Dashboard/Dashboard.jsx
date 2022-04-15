92bb87f2232d40b210731334d242e964bfdaf2a7
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-15 10:12:41 -0500

    Fix the className and change h1 for a div

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,24 +2,24 @@ import "./dashboard.css";
 
 const Dashboard = () => (
   <div className="dashboard">
-    <h3 className="dashboard__subtitle">Dashboard</h3>
-    <h1 className="dashboard__title">Bienvenid@s a Iconik</h1>
-    <div className="dashboard__card">
-      <div className="dashboard__resume dashboard__resume--primary-color">
+    <h1 className="dashboard__title">Dashboard</h1>
+    <div className="dashboard__text">Bienvenid@s a Iconik</div>
+    <div className="dashboard__resume">
+      <div className="dashboard__card dashboard__card--primary-color">
         <span className="dashboard__weight ">$</span>
         <span className="dashboard__value dashboard__value--signo">$<span className="dashboard__value">1200000</span></span>
         <h3 className="dashboard__label">Ingresos</h3>
       </div>
-      <div className="dashboard__resume dashboard__resume--secondary-color">
+      <div className="dashboard__card dashboard__card--secondary-color">
         <span className="dashboard__weight ">$</span>
         <span className="dashboard__value dashboard__value--signo">$<span className="dashboard__value">4000000</span></span>
         <h3  className="dashboard__label">Egresos</h3>
       </div>
-      <div className="dashboard__resume dashboard__resume--old-rose-color">
+      <div className="dashboard__card dashboard__card--old-rose-color">
         <span className="dashboard__value">5</span>
         <h3  className="dashboard__label">Clientes</h3>
       </div>
-      <div className="dashboard__resume dashboard__resume--third-color">
+      <div className="dashboard__card dashboard__card--third-color">
         <span className="dashboard__value">10</span>
         <h3  className="dashboard__label">Proximas Citas</h3>
       </div>
