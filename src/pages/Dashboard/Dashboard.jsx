9d2c60ce495431af44d2c13360f83a6e0e5993ec
Nicolle Montilla R <nicolle.montilla@gmail.com>
2022-04-15 10:31:50 -0500

    Change classNames on dashboard pages

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,22 +6,22 @@ const Dashboard = () => (
     <div className="dashboard__text">Bienvenid@s a Iconik</div>
     <div className="dashboard__resume">
       <div className="dashboard__card dashboard__card--primary-color">
-        <span className="dashboard__weight ">$</span>
-        <span className="dashboard__value dashboard__value--signo">$<span className="dashboard__value">1200000</span></span>
-        <h3 className="dashboard__label">Ingresos</h3>
+        <span className="dashboard__card-icon ">$</span>
+        <span className="dashboard__card-value dashboard__card-value--currency">$<span className="dashboard__value">1200000</span></span>
+        <h3 className="dashboard__card-label">Ingresos</h3>
       </div>
       <div className="dashboard__card dashboard__card--secondary-color">
-        <span className="dashboard__weight ">$</span>
-        <span className="dashboard__value dashboard__value--signo">$<span className="dashboard__value">4000000</span></span>
-        <h3  className="dashboard__label">Egresos</h3>
+        <span className="dashboard__card-icon ">$</span>
+        <span className="dashboard__card-value dashboard__card-value--currency">$<span className="dashboard__value">4000000</span></span>
+        <h3  className="dashboard__card-label">Egresos</h3>
       </div>
       <div className="dashboard__card dashboard__card--old-rose-color">
-        <span className="dashboard__value">5</span>
-        <h3  className="dashboard__label">Clientes</h3>
+        <span className="dashboard__card-value">5</span>
+        <h3  className="dashboard__card-label">Clientes</h3>
       </div>
       <div className="dashboard__card dashboard__card--third-color">
-        <span className="dashboard__value">10</span>
-        <h3  className="dashboard__label">Proximas Citas</h3>
+        <span className="dashboard__card-value">10</span>
+        <h3  className="dashboard__card-label">Proximas Citas</h3>
       </div>
     </div>
   </div>
